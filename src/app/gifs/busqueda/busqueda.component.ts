01c62f66b787d031afc08bac185f9e22a3160140
jreyarijon <reyarijonjose@gmail.com>
2021-08-26 16:32:43 +0200

    Funcion que recoje la busqueda

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent{
 
-  constructor() { }
+  // El ! asegura que el valor no va a ser nulo
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  buscar(){
+    //console.log(this.txtBuscar);
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+
+    this.txtBuscar.nativeElement.value = '';
   }
 
 }
